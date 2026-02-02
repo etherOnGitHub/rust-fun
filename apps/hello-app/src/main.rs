a81bba307d7dc5e4cac1649ed87c0fa4c624e900
etherchild <81761442+etherOnGitHub@users.noreply.github.com>
2026-02-02 13:12:15 +0000

    update: logic shuffled to save a name field, add: simple tests of logic

--- apps/hello-app/src/main.rs
+++ apps/hello-app/src/main.rs
@@ -2,14 +2,15 @@ use core::greeting::greeting;
 use validation::validation::validate_name;
 
 fn main() {
-    let message =  greeting("etherchild");
+    let name = "etherchild";
 
-    if let Err(error) = validate_name(&message) {
+    if let Err(error) = validate_name(name) {
         println!("Validation error: {}", error);
         return;
     } else {
         println!("Name is valid.");
     }
 
+    let message =  greeting("etherchild");
     println!("{}", message);
 }
