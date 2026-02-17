7f1460629aad481f8b0dbc30f4e44e96b2070a47
etherchild <81761442+etherOnGitHub@users.noreply.github.com>
2026-02-17 14:39:09 +0000

    some messing around

--- apps/hello-app/src/main.rs
+++ apps/hello-app/src/main.rs
@@ -1,9 +1,13 @@
+use std::mem;
+
 use core::greeting::greeting;
 use core::greeting::reverser;
 use core::greeting::even_numbers_total;
 use validation::validation::validate_name;
+use messingaround::alison::hiworld;
+use messingaround::alison::bool;
 
-pub fn main() {
+fn main() {
     let name = "etherchild";
 
     if let Err(error) = validate_name(name) {
@@ -30,4 +34,31 @@ pub fn main() {
     let numbers = [1, 2, 3, 4, 5, 6];
     let total = even_numbers_total(&numbers);
     println!("Total of even numbers: {}", total);
+
+    hiworld();
+    println!("Boolean value: {}", bool());
+
+    let c = "c";
+    let number = 42;
+    println!("I got this letter here: {} but I also got this stupid ahh number: {}", c, number);
+    let scope = "";
+    println!("Size of bool: {} bytes", mem::size_of_val(&bool));
+    {
+        let scope = "This is a new scope";
+        println!("Inside the scope: {}", scope);
+    }
+    if scope == "This is a new scope" {
+        println!("Scope is still accessible here: {}", scope);
+    } else {
+        println!("Scope is not accessible here.");
+    }
+
+    let n1 = 10;
+    let n2 = 20;
+    {
+        let n3 = n1 + n2;
+        println!("The sum of n1 and n2 is: {}", n3);
+    }
+    let n3 = n1 * n2;
+    println!("The product of n1 and n2 is: {}", n3);
 }
---
+++ crates/messingaround/src/alison.rs
@@ -0,0 +1,7 @@
+pub fn hiworld() {
+    println!("Hi, World! or something whatever");
+}
+
+pub fn bool() -> bool {
+    true
+}
